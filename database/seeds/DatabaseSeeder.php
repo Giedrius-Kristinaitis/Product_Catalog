f2e29fe24d066b99c08aa539a41efd60ee179329
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-10 23:35:14 +0300

    Added calls to data seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AccountSeeder::class);
+        $this->call(AccountRoleSeeder::class);
+        $this->call(AdminRoleSeeder::class);
     }
 }
