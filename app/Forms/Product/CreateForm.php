f91cae9f5a1580b826c0676a3d02ee846791b4bf
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-11 17:54:53 +0300

    Fixed field attributes that were not working

--- app/Forms/Product/CreateForm.php
+++ app/Forms/Product/CreateForm.php
@@ -16,12 +16,20 @@ class CreateForm extends Form
     {
         $this
             ->add('name', 'text', [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Name'
+                ],
                 'rules' => 'required',
                 'error_messages' => [
                     'name.required' => 'The name field is required!'
                 ]
             ])
             ->add('description', 'textarea', [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Description'
+                ],
                 'rules' => 'required',
                 'error_messages' => [
                     'description.required' => 'The description field is required!'
@@ -37,25 +45,36 @@ class CreateForm extends Form
                 'checked' => 'checked'
             ])
             ->add('sku', 'number', [
-                'min' => 0,
-                'step' => '1',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Stock keeping unit',
+                    'min' => 0,
+                    'step' => '1',
+                ],
                 'rules' => 'required|min:0',
                 'error_messages' => [
                     'sku.required' => 'The SKU field is required!'
                 ]
             ])
             ->add('base_price', 'number', [
-                'min' => 0,
-                'step' => 'any',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Base price',
+                    'min' => 0,
+                    'step' => 'any',
+                ],
                 'rules' => 'required|min:0',
                 'error_messages' => [
                     'base_price.required' => 'The base price field is required!'
                 ]
             ])
             ->add('special_price', 'number', [
-                'min' => 0,
-                'step' => 'any',
-                'placeholder' => 'Leave empty if the product has no special price'
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 0,
+                    'step' => 'any',
+                    'placeholder' => 'Leave empty if the product has no special price'
+                ]
             ])
             ->add('submit', 'submit', [
                 'value' => 'Create'
