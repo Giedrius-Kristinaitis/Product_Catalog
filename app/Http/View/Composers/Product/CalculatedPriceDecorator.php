9e6c8ecf4c6fd950f78643b8c113545032b638b4
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-14 20:06:51 +0300

    Created decorator to add calculated price

---
+++ app/Http/View/Composers/Product/CalculatedPriceDecorator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\View\Composers\Product;
+
+use App\Product;
+use App\Product\Price\PriceCalculator;
+
+class CalculatedPriceDecorator extends ProductComposerDecorator
+{
+    /**
+     * @var PriceCalculator
+     */
+    private $price_calculator;
+
+    /**
+     * CalculatedPriceDecorator constructor.
+     * @param ProductComposerInterface $product_composer
+     * @param PriceCalculator $price_calculator
+     */
+    public function __construct(ProductComposerInterface $product_composer, PriceCalculator $price_calculator)
+    {
+        parent::__construct($product_composer);
+
+        $this->price_calculator = $price_calculator;
+    }
+
+    /**
+     * Composes a product by adding additional calculated price to it
+     *
+     * @param Product $product
+     */
+    public function compose(Product $product): void
+    {
+        parent::compose($product);
+
+        $this->addCalculatedPriceToProduct($product);
+    }
+
+    /**
+     * Calculates price for a single product and adds it to it's model
+     *
+     * @param Product $product
+     */
+    private function addCalculatedPriceToProduct(Product $product): void
+    {
+        $product->calculated_price = $this->price_calculator->calculateProductPrice($product);
+    }
+}
