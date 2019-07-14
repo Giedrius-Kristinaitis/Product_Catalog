b0d06ff284ce041447fe9e6e1d4d70869053a3b3
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-14 20:11:15 +0300

    Refactored product composer for product grid

--- app/Http/View/Composers/CardProductComposer.php
+++ app/Http/View/Composers/CardProductComposer.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\View\Composers;
 
-use App\Product;
+use App\Http\View\Composers\Product\CalculatedPriceDecorator;
+use App\Http\View\Composers\Product\CalculatedPriceWithoutDiscountDecorator;
+use App\Http\View\Composers\Product\ImageUrlDecorator;
+use App\Http\View\Composers\Product\ProductComposer;
+use App\Http\View\Composers\Product\ProductComposerInterface;
+use App\Http\View\Composers\Product\RatingDecorator;
 use App\Product\Price\PriceCalculator;
 use App\Repository\ProductRepositoryInterface;
 use App\Utils\Url\UrlGeneratorInterface;
-use Illuminate\Support\Collection;
 
-class CardProductComposer extends ProductComposer
+/**
+ * Class CardProductComposer
+ *
+ * Composes products to be displayed in a product card
+ *
+ * @package App\Http\View\Composers
+ */
+class CardProductComposer extends BaseProductComposer
 {
     /**
      * CardProductComposer constructor.
-     *
      * @param PriceCalculator $price_calculator
      * @param ProductRepositoryInterface $product_repository
      * @param UrlGeneratorInterface $url_generator
@@ -23,68 +33,24 @@ class CardProductComposer extends ProductComposer
     }
 
     /**
-     * Modifies the given products by adding calculated price to them, generating
-     * public image url and adding rating value
+     * Gets the composer that will be applied to composed products
      *
-     * @return Collection
+     * @return ProductComposerInterface
      */
-    public function getModifiedProducts(): Collection
+    protected function getComposer(): ProductComposerInterface
     {
-        $modified_products = parent::getModifiedProducts();
-
-        $this->addRatingToAllProducts($modified_products);
-        $this->addNonDiscountCalculatedPriceToAllProducts($modified_products);
-
-        return $modified_products;
-    }
-
-    /**
-     * Adds calculated price to products without including discount
-     * @param Collection $products
-     */
-    private function addNonDiscountCalculatedPriceToAllProducts(Collection $products): void
-    {
-        foreach ($products as $product)
-        {
-            $this->addNonDiscountCalculatedPriceToProduct($product);
-        }
-    }
-
-    /**
-     * Adds calculated price to a single product without including discount
-     * @param Product $product
-     */
-    private function addNonDiscountCalculatedPriceToProduct(Product $product): void
-    {
-        $calculated_price_no_discount = $this->price_calculator->calculateProductPriceWithoutDiscount($product);
-
-        if ($calculated_price_no_discount > $product->calculated_price)
-        {
-            $product->calculated_price_no_discount = $calculated_price_no_discount;
-            $product->display_no_discount_price = true;
-        }
-    }
-
-    /**
-     * Adds rating value to all products
-     *
-     * @param Collection $products
-     */
-    private function addRatingToAllProducts(Collection $products): void
-    {
-        foreach ($products as $product)
-        {
-            $this->addRatingToProduct($product);
-        }
-    }
-
-    /**
-     * Adds rating value to a single product
-     *
-     * @param Product $product
-     */
-    private function addRatingToProduct(Product $product): void
-    {
-        $product->rating = $this->product_repository->getRating($product->id);
+        return new ImageUrlDecorator(
+            new CalculatedPriceDecorator(
+                new CalculatedPriceWithoutDiscountDecorator(
+                    new RatingDecorator(
+                        new ProductComposer(),
+                        $this->product_repository
+                    ),
+                    $this->price_calculator
+                ),
+                $this->price_calculator
+            ),
+            $this->url_generator
+        );
     }
 }
