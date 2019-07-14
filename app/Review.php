9edb1d4dbefe2c3e99968425f710c703c12df6e1
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-15 00:20:11 +0300

    Added fillable fields

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'reviewer', 'review', 'rating', 'product_id'
+    ];
 }
