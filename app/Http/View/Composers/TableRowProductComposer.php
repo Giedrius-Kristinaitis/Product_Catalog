b40469a374e8b9444c862cd0fc06ab6bf4c45fc0
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-14 20:10:45 +0300

    Refactored product composer for dashboard's table

--- app/Http/View/Composers/TableRowProductComposer.php
+++ app/Http/View/Composers/TableRowProductComposer.php
@@ -2,114 +2,47 @@
 
 namespace App\Http\View\Composers;
 
-use App\Product;
+use App\Http\View\Composers\Product\CalculatedPriceDecorator;
+use App\Http\View\Composers\Product\ImageUrlDecorator;
+use App\Http\View\Composers\Product\ProductComposer;
+use App\Http\View\Composers\Product\ProductComposerInterface;
 use App\Product\Price\PriceCalculator;
 use App\Repository\ProductRepositoryInterface;
 use App\Utils\Url\UrlGeneratorInterface;
-use Illuminate\Support\Collection;
-use Illuminate\View\View;
 
-class ProductComposer
+/**
+ * Class TableRowProductComposer
+ *
+ * Composes products to be displayed in a table row
+ *
+ * @package App\Http\View\Composers
+ */
+class TableRowProductComposer extends BaseProductComposer
 {
     /**
-     * @var PriceCalculator Calculates product prices
-     */
-    protected $price_calculator;
-
-    /**
-     * @var ProductRepositoryInterface
-     */
-    protected $product_repository;
-
-    /**
-     * @var UrlGeneratorInterface
-     */
-    protected $url_generator;
-
-    /**
-     * Create a new profile composer.
-     *
+     * TableRowProductComposer constructor.
      * @param PriceCalculator $price_calculator
      * @param ProductRepositoryInterface $product_repository
      * @param UrlGeneratorInterface $url_generator
      */
     public function __construct(PriceCalculator $price_calculator, ProductRepositoryInterface $product_repository, UrlGeneratorInterface $url_generator)
     {
-        $this->price_calculator = $price_calculator;
-        $this->product_repository = $product_repository;
-        $this->url_generator = $url_generator;
+        parent::__construct($price_calculator, $product_repository, $url_generator);
     }
 
     /**
-     * Bind data to the view.
+     * Gets the composer that will be applied to composed products
      *
-     * @param  View  $view
-     * @return void
+     * @return ProductComposerInterface
      */
-    public function compose(View $view): void
+    protected function getComposer(): ProductComposerInterface
     {
-        $view->with('products', $this->getModifiedProducts());
-    }
-
-    /**
-     * Modifies the given products by adding calculated price to them and generating
-     * public image url
-     *
-     * @return Collection
-     */
-    protected function getModifiedProducts(): Collection
-    {
-        $products = $this->product_repository->all();
-
-        $this->addCalculatedPriceToAllProducts($products);
-        $this->createImageUrlForAllProducts($products);
-
-        return $products;
-    }
-
-    /**
-     * Creates image urls for the specified products
-     *
-     * @param Collection $products
-     */
-    protected function createImageUrlForAllProducts(Collection $products): void
-    {
-        foreach ($products as $product)
-        {
-            $this->createImageUrlForProduct($product);
-        }
-    }
-
-    /**
-     * Creates image url for a product
-     *
-     * @param Product $product
-     */
-    private function createImageUrlForProduct(Product $product): void
-    {
-        $product->image = $this->url_generator->generatePublicUrl($product->image);
-    }
-
-    /**
-     * Calculates price for each product and adds it to the model
-     *
-     * @param Collection $products
-     */
-    protected function addCalculatedPriceToAllProducts(Collection $products): void
-    {
-        foreach ($products as $product)
-        {
-            $this->addCalculatedPriceToProduct($product);
-        }
-    }
-
-    /**
-     * Calculates price for a single product and adds it to it's model
-     *
-     * @param Product $product
-     */
-    private function addCalculatedPriceToProduct(Product $product): void
-    {
-        $product->calculated_price = $this->price_calculator->calculateProductPrice($product);
+        return new ImageUrlDecorator(
+            new CalculatedPriceDecorator(
+                new ProductComposer(),
+                $this->price_calculator
+            ),
+            $this->url_generator
+        );
     }
 }
